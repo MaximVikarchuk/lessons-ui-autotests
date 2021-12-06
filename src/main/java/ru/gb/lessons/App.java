e998f0a1b8429b31e73e65ed2eabab2776358d58
Maksim Vikarchuk <m.vikarchuk@xpert.ch>
2021-12-06 14:29:58 +0300

    123

--- src/main/java/ru/gb/lessons/App.java
+++ src/main/java/ru/gb/lessons/App.java
@@ -1,13 +1,36 @@
-package ru.gb.lessons;
-
-/**
- * Hello world!
- *
- */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
-    }
-}
+//package ru.gb.lessons;
+//
+//import io.github.bonigarcia.wdm.WebDriverManager;
+//import org.openqa.selenium.By;
+//import org.openqa.selenium.Dimension;
+//import org.openqa.selenium.WebDriver;
+//import org.openqa.selenium.WebElement;
+//import org.openqa.selenium.chrome.ChromeDriver;
+//import org.openqa.selenium.interactions.Actions;
+//
+//
+//public class App
+//{
+//    public static void main( String[] args ) throws InterruptedException {
+//
+//        WebDriver webDriver = WebDriverManager.chromedriver().create();
+//
+//        webDriver.get("https://crm.geekbrains.space/");
+//
+//        webDriver.manage().window().setSize(new Dimension(1500, 720));
+//
+//        webDriver.findElement(By.name("_username")).sendKeys("Applanatest1");
+//        webDriver.findElement(By.name("_password")).sendKeys("Student2020!");
+//        webDriver.findElement(By.name("_submit")).click();
+//
+//        Actions actions = new Actions();
+//
+//
+//        Thread.sleep(10000);
+//
+//        webDriver.quit();
+//
+//    }
+//
+//
+//}
